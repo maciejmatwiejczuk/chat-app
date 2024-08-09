9201355563a63a6a420218e9bee5f8d42d7d8fd3
Maciej Matwiejczuk <138388056+maciejmatwiejczuk@users.noreply.github.com>
2024-08-09 16:44:08 +0200

    Added listener for 'chat-message' on the server
    Also 'socket.io was' installed

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -1,11 +1,21 @@
 import express from 'express';
+import { createServer } from 'node:http';
+import { Server } from 'socket.io';
 
 const app = express();
+const server = createServer(app);
+const io = new Server(server);
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-app.listen(8080, () => {
+io.on('connection', (socket) => {
+  socket.on('chat-message', (msg) => {
+    io.emit('chat-message', msg);
+  });
+});
+
+server.listen(8080, () => {
   console.log('Server listening at http://localhost:8080');
 });
