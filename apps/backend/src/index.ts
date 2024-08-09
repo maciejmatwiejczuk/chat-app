0b5a70868392a61803b06f9566a32e88d956f260
Maciej Matwiejczuk <138388056+maciejmatwiejczuk@users.noreply.github.com>
2024-08-09 17:09:40 +0200

    Enable cors on the server

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -4,7 +4,11 @@ import { Server } from 'socket.io';
 
 const app = express();
 const server = createServer(app);
-const io = new Server(server);
+const io = new Server(server, {
+  cors: {
+    origin: 'http://localhost:3000',
+  },
+});
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
