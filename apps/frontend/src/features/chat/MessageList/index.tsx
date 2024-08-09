1e87d6b2e6384cd5f163f97d1eb99f413c7f239f
Maciej Matwiejczuk <138388056+maciejmatwiejczuk@users.noreply.github.com>
2024-08-09 17:30:46 +0200

    Render chat messages in 'MessageList' component

--- apps/frontend/src/features/chat/MessageList/index.tsx
+++ apps/frontend/src/features/chat/MessageList/index.tsx
@@ -1,5 +1,17 @@
-function MessageList() {
-  return <></>;
+interface MessageListProps {
+  chatMessages: Array<string>;
+}
+
+function MessageList({ chatMessages }: MessageListProps) {
+  return (
+    chatMessages && (
+      <ul>
+        {chatMessages.map((msg) => (
+          <li>{msg}</li>
+        ))}
+      </ul>
+    )
+  );
 }
 
 export default MessageList;
