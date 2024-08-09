4ba140fb3bead081e03aed33b72629bc0469b608
Maciej Matwiejczuk <138388056+maciejmatwiejczuk@users.noreply.github.com>
2024-08-09 15:37:53 +0200

    Add JSX and styles to 'MessageInput' component
    Also installed '@phosphor-icons/react'

--- apps/frontend/src/features/chat/MessageInput/index.tsx
+++ apps/frontend/src/features/chat/MessageInput/index.tsx
@@ -1,5 +1,19 @@
+import { PaperPlaneTilt } from '@phosphor-icons/react';
+import styles from './styles.module.css';
+
 function MessageInput() {
-  return <></>;
+  return (
+    <div className={styles.container}>
+      <input
+        className={styles.inputBox}
+        type="text"
+        placeholder="Write a message"
+      />
+      <button className={styles.sendButton}>
+        <PaperPlaneTilt size={24} weight="fill" color="#fff" />
+      </button>
+    </div>
+  );
 }
 
 export default MessageInput;
