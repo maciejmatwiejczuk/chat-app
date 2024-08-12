d73973579c17cd7541f447b49467b2b7be658d00
Maciej Matwiejczuk <138388056+maciejmatwiejczuk@users.noreply.github.com>
2024-08-12 18:36:34 +0200

    Create helper function to group message by date
    Grouping of the messages is based on time interval

---
+++ apps/frontend/src/helpers/chat.helpers.ts
@@ -0,0 +1,36 @@
+import { ChatMessage } from '../App';
+
+/**
+ * This helper function puts messages into the map
+ * grouping them by the date key depending on the time
+ * interval between them i.e. if the time interval
+ * between messages is more than passed argument,
+ * the next message creates another group
+ */
+export function groupMessagesByTime(
+  messages: Array<ChatMessage>,
+  timeIntervalInMinutes: number = 15
+) {
+  const messagesGrouped = new Map<Date, Array<ChatMessage>>();
+
+  let dateKey = messages[0].date;
+  let messagesUnderDateKey = [messages[0]];
+
+  for (let i = 1; i < messages.length; i++) {
+    const timeDifference =
+      (messages[i].date.getTime() - messages[i - 1].date.getTime()) /
+      (1000 * 60);
+
+    if (timeDifference > timeIntervalInMinutes) {
+      messagesGrouped.set(dateKey, messagesUnderDateKey);
+
+      dateKey = messages[i].date;
+      messagesUnderDateKey = [messages[i]];
+    } else {
+      messagesUnderDateKey.push(messages[i]);
+    }
+  }
+  messagesGrouped.set(dateKey, messagesUnderDateKey);
+
+  return messagesGrouped;
+}
