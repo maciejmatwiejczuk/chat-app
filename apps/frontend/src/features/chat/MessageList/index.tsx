2e9ff80ee3b42019e08d6da85e643f18195d7341
Maciej Matwiejczuk <138388056+maciejmatwiejczuk@users.noreply.github.com>
2024-08-14 00:24:44 +0200

    Display messages with date labels in MessageList

--- apps/frontend/src/features/chat/MessageList/index.tsx
+++ apps/frontend/src/features/chat/MessageList/index.tsx
@@ -1,18 +1,46 @@
+import { ReactElement } from 'react';
 import { ChatMessage } from '../../../App';
+import { groupMessagesByTime } from '../../../helpers/chat.helpers';
+import { formatDate } from '../../../helpers/date.helpers';
+import { v4 as uuid } from 'uuid';
 
 interface MessageListProps {
   chatMessages: Array<ChatMessage>;
 }
 
 function MessageList({ chatMessages }: MessageListProps) {
+  function renderMessages() {
+    const chatMessagesMap = chatMessages && groupMessagesByTime(chatMessages);
+
+    const messageGroups: Array<
+      ReactElement<MessageGroupProps, typeof MessageGroup>
+    > = [];
+
+    for (const [date, messages] of chatMessagesMap) {
+      messageGroups.push(
+        <MessageGroup key={uuid()} date={date} messages={messages} />
+      );
+    }
+
+    return messageGroups;
+  }
+
+  return chatMessages.length > 0 && renderMessages();
+}
+
+interface MessageGroupProps {
+  date: Date;
+  messages: Array<ChatMessage>;
+}
+
+function MessageGroup({ date, messages }: MessageGroupProps) {
   return (
-    chatMessages && (
-      <ul>
-        {chatMessages.map((msgObj) => (
-          <li>{msgObj.message}</li>
-        ))}
-      </ul>
-    )
+    <>
+      <p>{formatDate(date)}</p>
+      {messages.map((msg) => (
+        <li key={msg.id}>{msg.message}</li>
+      ))}
+    </>
   );
 }
 
