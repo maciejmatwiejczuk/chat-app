716b04da3304ed42d4c5bee4f7b2ee3d2a9ce084
Maciej Matwiejczuk <138388056+maciejmatwiejczuk@users.noreply.github.com>
2024-09-06 14:31:03 +0200

    Modify layout to accomodate 'List' component
    * Rendered 'List' inside 'App'
    * Created css file for 'App' component
    * Modified css for 'Chat' and index.css (global css file)

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -1,10 +1,13 @@
 import Chat from './components/chat/index.tsx';
+import List from './components/List/index.tsx';
+import styles from './app.module.css';
 
 function App() {
   return (
-    <>
+    <div className={styles.container}>
+      <List />
       <Chat />
-    </>
+    </div>
   );
 }
 
