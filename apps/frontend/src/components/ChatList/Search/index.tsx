0cef68e8ffc8ac0ce36fdae659527ce5ce877a86
Maciej Matwiejczuk <138388056+maciejmatwiejczuk@users.noreply.github.com>
2024-09-08 20:47:02 +0200

    Update 'Search' component
    Modified 'Search' so it's now a controller component.

--- apps/frontend/src/components/ChatList/Search/index.tsx
+++ apps/frontend/src/components/ChatList/Search/index.tsx
@@ -1,11 +1,22 @@
 import { MagnifyingGlass } from '@phosphor-icons/react';
 import styles from './styles.module.css';
 
-function Search() {
+interface SearchProps {
+  value: string;
+  setValue: React.Dispatch<React.SetStateAction<string>>;
+}
+
+function Search({ value, setValue }: SearchProps) {
   return (
     <div className={styles.container}>
       <MagnifyingGlass size={24} weight="bold" className={styles.searchIcon} />
-      <input type="text" className={styles.input} placeholder="Search" />
+      <input
+        type="text"
+        className={styles.input}
+        placeholder="Search"
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+      />
     </div>
   );
 }
