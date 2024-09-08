ac905538e5ab9c7d630f2a9d8a69edc80fb12b8a
Maciej Matwiejczuk <138388056+maciejmatwiejczuk@users.noreply.github.com>
2024-09-08 14:06:49 +0200

    Create 'Dropdown' component

---
+++ apps/frontend/src/components/List/Dropdown/index.tsx
@@ -0,0 +1,47 @@
+import { useState } from 'react';
+import { CaretUp } from '@phosphor-icons/react';
+import styles from './styles.module.css';
+
+interface DropdownProps {
+  options: { icon: JSX.Element; title: string; value: string }[];
+  onSelect: React.Dispatch<React.SetStateAction<string>>;
+}
+
+function Dropdown({ options, onSelect }: DropdownProps) {
+  const [isOpen, setIsOpen] = useState(false);
+  const [selection, setSelection] = useState<JSX.Element | string>(
+    options[0].icon
+  );
+
+  return (
+    <div className={styles.container}>
+      <button
+        onClick={() => setIsOpen(!isOpen)}
+        className={isOpen ? styles.trigger : styles.triggerOpened}
+      >
+        <CaretUp size={16} weight="bold" className={styles.caretIcon} />
+        {selection}
+      </button>
+      {isOpen && (
+        <ul className={styles.menu}>
+          {options.map((option) => (
+            <li className={styles.menuItem}>
+              <button
+                onClick={() => {
+                  setSelection(option.icon);
+                  onSelect(option.value);
+                  setIsOpen(!isOpen);
+                }}
+              >
+                {option.icon}
+                {option.title}
+              </button>
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
+}
+
+export default Dropdown;
