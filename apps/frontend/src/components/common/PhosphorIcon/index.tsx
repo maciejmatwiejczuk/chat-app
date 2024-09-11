d84977e441443dbcd399795fa6832a04231ceec9
Maciej Matwiejczuk <138388056+maciejmatwiejczuk@users.noreply.github.com>
2024-09-11 20:43:46 +0200

    Create generic icon component 'PhosphorIcon'
    The component allows to render icon providing icon name as a prop
    which is useful for reusable components that require specific icon
    design.

---
+++ apps/frontend/src/components/common/PhosphorIcon/index.tsx
@@ -0,0 +1,40 @@
+import { lazy, Suspense, forwardRef } from 'react';
+import type { IconProps } from '@phosphor-icons/react';
+
+type ImportMap = Record<
+  string,
+  (name: string) => Promise<{ default: React.ComponentType<IconProps> }>
+>;
+
+const importMap: ImportMap = Object.entries(
+  import.meta.glob(
+    '../../../../../../node_modules/@phosphor-icons/react/dist/ssr/*.mjs'
+  )
+).reduce((acc, [path, fn]) => {
+  acc[path] = (name: string) =>
+    fn().then((mod) => {
+      const typedMod = mod as Record<string, React.ComponentType<IconProps>>;
+      return { default: typedMod[name] };
+    });
+  return acc;
+}, {} as ImportMap);
+
+function relativePathForIcon(name: string): string {
+  return `../../../../../../node_modules/@phosphor-icons/react/dist/ssr/${name}.mjs`;
+}
+
+const PhosphorIcon = forwardRef<SVGSVGElement, IconProps & { name: string }>(
+  (props, ref) => {
+    const { name, ...iconProps } = props;
+
+    const Icon = lazy(() => importMap[relativePathForIcon(name)](name));
+
+    return (
+      <Suspense fallback={null}>
+        <Icon {...iconProps} ref={ref} />
+      </Suspense>
+    );
+  }
+);
+
+export default PhosphorIcon;
