84c3fc60c9686eaa41f0daba09029de1d9af7e84
Maciej Matwiejczuk <138388056+maciejmatwiejczuk@users.noreply.github.com>
2024-09-11 23:58:31 +0200

    Create 'SignIn' page

---
+++ apps/frontend/src/pages/SignIn/index.tsx
@@ -0,0 +1,40 @@
+import { useState } from 'react';
+import TextInput from '../../components/common/TextInput';
+import styles from './styles.module.css';
+
+function SignIn() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  return (
+    <div className={styles.container}>
+      <form className={styles.form}>
+        <h2 className={styles.heading}>Sign In</h2>
+
+        <div className={styles.inputWrapper}>
+          <TextInput value={username} setValue={setUsername} label="Username" />
+        </div>
+        <div className={styles.inputWrapper}>
+          <TextInput
+            value={password}
+            setValue={setPassword}
+            label="Password"
+            type="password"
+          />
+        </div>
+
+        <button className={styles.submit}>Sign In</button>
+
+        <p className={styles.bottomText}>
+          Don't have an account yet?{' '}
+          <a href="#" className={styles.redirect}>
+            Sign up here
+          </a>
+          .
+        </p>
+      </form>
+    </div>
+  );
+}
+
+export default SignIn;
