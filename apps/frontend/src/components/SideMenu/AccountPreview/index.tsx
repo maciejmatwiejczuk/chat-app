126fd16a134ab7765248df6e111a46f596bfafd0
Maciej Matwiejczuk <138388056+maciejmatwiejczuk@users.noreply.github.com>
2024-09-14 01:21:42 +0200

    Use 'Modal' in 'AccountPreview'

--- apps/frontend/src/components/SideMenu/AccountPreview/index.tsx
+++ apps/frontend/src/components/SideMenu/AccountPreview/index.tsx
@@ -1,14 +1,38 @@
+import { useState } from 'react';
 import Avatar from '../../common/Avatar';
+import Modal from '../../common/Modal';
 import styles from './styles.module.css';
 
 function AccountPreview() {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  function openModal() {
+    setIsModalOpen(true);
+  }
+
+  function closeModal() {
+    setIsModalOpen(false);
+  }
+
   return (
     <div className={styles.container}>
       <div className={styles.usernameAvatarWrapper}>
         <Avatar />
         <h3 className={styles.username}>Username</h3>
       </div>
-      <button className={styles.logout}>Log out</button>
+      <button className={styles.logout} onClick={openModal}>
+        Log out
+      </button>
+      {isModalOpen && (
+        <Modal isOpen={isModalOpen} onClose={closeModal}>
+          <h2 className={styles.modalTitle}>Log out</h2>
+          <p className={styles.modalText}>Are you sure you want to log out?</p>
+          <div className={styles.buttonWrapper}>
+            <button className={styles.buttonNo}>No</button>
+            <button className={styles.buttonYes}>Yes</button>
+          </div>
+        </Modal>
+      )}
     </div>
   );
 }
