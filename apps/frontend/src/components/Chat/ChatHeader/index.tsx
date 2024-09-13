c8a1c706aef73c758c4984827ca122ad4879b60d
Maciej Matwiejczuk <138388056+maciejmatwiejczuk@users.noreply.github.com>
2024-09-13 18:25:26 +0200

    Modify 'ChatHeader'
    Display avatar, username and status (for now it's static).

--- apps/frontend/src/components/Chat/ChatHeader/index.tsx
+++ apps/frontend/src/components/Chat/ChatHeader/index.tsx
@@ -1,7 +1,16 @@
+import Avatar from '../../common/Avatar';
 import styles from './styles.module.css';
 
 function ChatHeader() {
-  return <div className={styles.container}>HEADER PLACEHOLDER</div>;
+  return (
+    <div className={styles.container}>
+      <Avatar />
+      <div>
+        <h3 className={styles.username}>Username</h3>
+        <p className={styles.status}>Online</p>
+      </div>
+    </div>
+  );
 }
 
 export default ChatHeader;
