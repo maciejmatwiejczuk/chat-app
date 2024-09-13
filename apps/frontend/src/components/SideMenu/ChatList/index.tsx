a0e80001d55fcb152d82862c4e99bb8c81a35f8b
Maciej Matwiejczuk <138388056+maciejmatwiejczuk@users.noreply.github.com>
2024-09-13 18:44:09 +0200

    Create 'ChatList' component
    The part of 'SideMenu' (old 'ChatList') responsible for rendering list
    of chats was moved here.

---
+++ apps/frontend/src/components/SideMenu/ChatList/index.tsx
@@ -0,0 +1,111 @@
+import { useParams, Link } from 'react-router-dom';
+import { MagnifyingGlass } from '@phosphor-icons/react';
+import { contacts, users } from '../../../data';
+import Avatar from '../../common/Avatar';
+import type { OptionValue } from '..';
+import styles from './styles.module.css';
+
+interface ChatListProps {
+  dropdownSelection: OptionValue;
+  searchValue: string;
+}
+
+function ChatList({ dropdownSelection, searchValue }: ChatListProps) {
+  const { chatId } = useParams();
+
+  function isChatOpen(id: number) {
+    return Number(chatId) === id;
+  }
+
+  function renderItems() {
+    if (dropdownSelection === 'my_contacts') {
+      if (searchValue) {
+        const foundContacts = contacts.filter((contact) =>
+          contact.username.toLowerCase().includes(searchValue.toLowerCase())
+        );
+
+        return foundContacts.map((contact) => (
+          <ChatListItem
+            socketId={contact.socketId}
+            name={contact.username}
+            lastMessage={contact.last_message}
+            isOpen={isChatOpen(contact.socketId)}
+          />
+        ));
+      }
+
+      return contacts.map((contact) => (
+        <ChatListItem
+          socketId={contact.socketId}
+          name={contact.username}
+          lastMessage={contact.last_message}
+          isOpen={isChatOpen(contact.socketId)}
+        />
+      ));
+    } else {
+      if (searchValue) {
+        const foundUsers = users
+          .filter((user) => !contacts.find((contact) => contact.id === user.id))
+          .filter((user) =>
+            user.username.toLowerCase().includes(searchValue.toLowerCase())
+          );
+
+        return foundUsers.map((contact) => (
+          <ChatListItem
+            socketId={contact.socketId}
+            name={contact.username}
+            isOpen={isChatOpen(contact.socketId)}
+          />
+        ));
+      }
+
+      return null;
+    }
+  }
+
+  return (
+    <div>
+      {dropdownSelection === 'find_users' && !searchValue ? (
+        <p className={styles.placeholder}>
+          Use{' '}
+          <MagnifyingGlass weight="bold" className={styles.placeholderIcon} />{' '}
+          <span>search</span> above to find users
+        </p>
+      ) : (
+        <ul className={styles.list}>{renderItems()}</ul>
+      )}
+    </div>
+  );
+}
+
+interface ChatListItemProps {
+  socketId: number;
+  name: string;
+  lastMessage?: string;
+  isOpen: boolean;
+}
+
+function ChatListItem({
+  socketId,
+  name,
+  lastMessage,
+  isOpen,
+}: ChatListItemProps) {
+  const listItemStyle = isOpen
+    ? `${styles.listItem} ${styles.listItemOpen}`
+    : styles.listItem;
+
+  return (
+    <li className={listItemStyle}>
+      <Link to={`chat/${socketId}`}>
+        <Avatar />
+        <div>
+          <h4 className={styles.chatName}>{name}</h4>
+          <p className={styles.lastMessage}>{lastMessage}</p>
+        </div>
+      </Link>
+    </li>
+  );
+}
+
+export default ChatList;
--- apps/frontend/src/components/SideMenu/index.tsx
+++ apps/frontend/src/components/SideMenu/index.tsx
@@ -1,15 +1,8 @@
 import { useState } from 'react';
-import { useParams } from 'react-router-dom';
-import {
-  AddressBook,
-  Binoculars,
-  MagnifyingGlass,
-} from '@phosphor-icons/react';
-import { Link } from 'react-router-dom';
+import { AddressBook, Binoculars } from '@phosphor-icons/react';
+import ChatList from './ChatList';
 import Dropdown from './Dropdown';
-import { users, contacts } from '../../data';
 import TextInput from '../common/TextInput';
-import Avatar from '../common/Avatar';
 import styles from './styles.module.css';
 
 export type OptionValue = 'my_contacts' | 'find_users';
@@ -39,58 +32,6 @@ function SideMenu() {
   );
   const [searchValue, setSearchValue] = useState('');
 
-  const { chatId } = useParams();
-
-  function isChatOpen(id: number) {
-    return Number(chatId) === id;
-  }
-
-  function renderItems() {
-    if (dropdownSelection === 'my_contacts') {
-      if (searchValue) {
-        const foundContacts = contacts.filter((contact) =>
-          contact.username.toLowerCase().includes(searchValue.toLowerCase())
-        );
-
-        return foundContacts.map((contact) => (
-          <ChatListItem
-            socketId={contact.socketId}
-            name={contact.username}
-            lastMessage={contact.last_message}
-            isOpen={isChatOpen(contact.socketId)}
-          />
-        ));
-      }
-
-      return contacts.map((contact) => (
-        <ChatListItem
-          socketId={contact.socketId}
-          name={contact.username}
-          lastMessage={contact.last_message}
-          isOpen={isChatOpen(contact.socketId)}
-        />
-      ));
-    } else {
-      if (searchValue) {
-        const foundUsers = users
-          .filter((user) => !contacts.find((contact) => contact.id === user.id))
-          .filter((user) =>
-            user.username.toLowerCase().includes(searchValue.toLowerCase())
-          );
-
-        return foundUsers.map((contact) => (
-          <ChatListItem
-            socketId={contact.socketId}
-            name={contact.username}
-            isOpen={isChatOpen(contact.socketId)}
-          />
-        ));
-      }
-
-      return null;
-    }
-  }
-
   return (
     <div className={styles.container}>
       <div className={styles.header}>
@@ -106,46 +47,11 @@ function SideMenu() {
           />
         </div>
       </div>
-      {dropdownSelection === 'find_users' && !searchValue ? (
-        <p className={styles.placeholder}>
-          Use{' '}
-          <MagnifyingGlass weight="bold" className={styles.placeholderIcon} />{' '}
-          <span>search</span> above to find users
-        </p>
-      ) : (
-        <ul className={styles.list}>{renderItems()}</ul>
-      )}
+      <ChatList
+        dropdownSelection={dropdownSelection}
+        searchValue={searchValue}
+      />
     </div>
-  );
-}
-
-interface ChatListItemProps {
-  socketId: number;
-  name: string;
-  lastMessage?: string;
-  isOpen: boolean;
-}
-
-function ChatListItem({
-  socketId,
-  name,
-  lastMessage,
-  isOpen,
-}: ChatListItemProps) {
-  const listItemStyle = isOpen
-    ? `${styles.listItem} ${styles.listItemOpen}`
-    : styles.listItem;
-
-  return (
-    <li className={listItemStyle}>
-      <Link to={`chat/${socketId}`}>
-        <Avatar />
-        <div>
-          <h4 className={styles.chatName}>{name}</h4>
-          <p className={styles.lastMessage}>{lastMessage}</p>
-        </div>
-      </Link>
-    </li>
   );
 }
 
