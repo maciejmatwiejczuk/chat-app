84bb09e93dfa05ab3ebae7b5dcb6fec7ba40442b
Maciej Matwiejczuk <138388056+maciejmatwiejczuk@users.noreply.github.com>
2024-09-14 01:21:02 +0200

    Create 'Modal' component

---
+++ apps/frontend/src/components/common/Modal/index.tsx
@@ -0,0 +1,35 @@
+import { PropsWithChildren } from 'react';
+import { X } from '@phosphor-icons/react';
+import styles from './styles.module.css';
+
+interface ModalProps {
+  isOpen: boolean;
+  onClose: () => void;
+}
+
+function Modal({ isOpen, onClose, children }: PropsWithChildren<ModalProps>) {
+  function onOverlayClick(e: React.MouseEvent<HTMLDivElement>) {
+    if (e.target === e.currentTarget) {
+      onClose();
+    }
+  }
+
+  return (
+    <>
+      {isOpen && (
+        <div className={styles.overlay} onClick={onOverlayClick}>
+          <div className={styles.content}>
+            <div className={styles.closeWrapper}>
+              <button className={styles.close} onClick={onClose}>
+                <X size={24} weight="bold" />
+              </button>
+            </div>
+            {children}
+          </div>
+        </div>
+      )}
+    </>
+  );
+}
+
+export default Modal;
