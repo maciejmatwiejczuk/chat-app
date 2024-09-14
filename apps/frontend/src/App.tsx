e53165db5f3e3eb35dbe8ac7e2f219adbabe3427
Maciej Matwiejczuk <138388056+maciejmatwiejczuk@users.noreply.github.com>
2024-09-14 21:01:46 +0200

    Delete App.tsx and corresponding css file
    Deleted because it's not needed anymore.

--- apps/frontend/src/App.tsx
+++
@@ -1,9 +0,0 @@
-import ChatPage from './pages/ChatPage/ChatPage';
-// import SignUp from './pages/SignUp';
-// import SignIn from './pages/SignIn';
-
-function App() {
-  return <ChatPage />;
-}
-
-export default App;
