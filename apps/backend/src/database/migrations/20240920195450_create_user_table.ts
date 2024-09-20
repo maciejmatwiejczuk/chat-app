f02347947ede403f246ed59efd41f61c63c6a655
Maciej Matwiejczuk <138388056+maciejmatwiejczuk@users.noreply.github.com>
2024-09-21 00:10:07 +0200

    Create first migration
    This migration creates 'user' table in the database

---
+++ apps/backend/src/database/migrations/20240920195450_create_user_table.ts
@@ -0,0 +1,15 @@
+import type { Kysely } from 'kysely';
+import type { Database } from '../types.js';
+
+export async function up(db: Kysely<Database>): Promise<void> {
+  await db.schema
+    .createTable('user')
+    .addColumn('id', 'integer', (col) => col.primaryKey())
+    .addColumn('username', 'text', (col) => col.notNull().unique())
+    .addColumn('password', 'text', (col) => col.notNull())
+    .execute();
+}
+
+export async function down(db: Kysely<Database>): Promise<void> {
+  await db.schema.dropTable('user').execute();
+}
