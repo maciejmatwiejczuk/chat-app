a552a6f03a9a4426868a23126fa6bb8527efbe23
Maciej Matwiejczuk <138388056+maciejmatwiejczuk@users.noreply.github.com>
2024-09-21 23:33:09 +0200

    Use user repository in user route handlers

--- apps/backend/src/modules/users/users.routes.ts
+++ apps/backend/src/modules/users/users.routes.ts
@@ -1,12 +1,13 @@
 import { Router } from 'express';
 import type { Request, Response } from 'express';
 import { UserSchema } from './users.schemas.js';
+import * as UserRepository from './users.repository.js';
 
 const router = Router();
 
 router
   .route('/')
-  .post((req: Request, res: Response) => {
+  .post(async (req: Request, res: Response) => {
     const { body: user } = req;
 
     const result = UserSchema.safeParse(user);
@@ -19,20 +20,42 @@ router
       });
     }
 
-    return res
-      .status(201)
-      .send({ success: true, message: 'New user created successfuly' });
+    const userToInsert = { ...user };
+    delete userToInsert.confirmPassword;
+
+    const createdUser = await UserRepository.createUser(userToInsert);
+
+    return res.status(201).send({
+      success: true,
+      message: 'New user created successfuly',
+      data: { created: { user: createdUser } },
+    });
   })
-  .get((req: Request, res: Response) => {
-    res.send({ message: 'Users returned successfuly' });
+  .get(async (req: Request, res: Response) => {
+    const users = await UserRepository.findUsers({});
+
+    res.send({
+      success: true,
+      message: 'Users returned successfuly',
+      data: { users },
+    });
   });
 
 router
   .route('/:id')
-  .get((req: Request, res: Response) => {
-    res.send({ message: 'User with specifies id returned successfuly' });
+  .get(async (req: Request, res: Response) => {
+    const id = Number(req.params.id);
+
+    const user = await UserRepository.findUserById(id);
+
+    res.send({
+      success: true,
+      message: 'User with specifies id returned successfuly',
+      data: { user },
+    });
   })
-  .patch((req: Request, res: Response) => {
+  .patch(async (req: Request, res: Response) => {
+    const id = Number(req.params.id);
     const { body: user } = req;
 
     const result = UserSchema.partial().safeParse(user);
@@ -45,10 +68,24 @@ router
       });
     }
 
-    return res.send({ success: true, message: 'User updated successfuly' });
+    const updatedUser = await UserRepository.updateUser(id, user);
+
+    return res.send({
+      success: true,
+      message: 'User updated successfuly',
+      data: { updated: { user: updatedUser } },
+    });
   })
-  .delete((req: Request, res: Response) => {
-    res.send({ message: 'User deleted successfuly' });
+  .delete(async (req: Request, res: Response) => {
+    const id = Number(req.params.id);
+
+    const deletedUser = await UserRepository.deleteUser(id);
+
+    res.send({
+      success: true,
+      message: 'User deleted successfuly',
+      data: { deleted: { user: deletedUser } },
+    });
   });
 
 export default router;
