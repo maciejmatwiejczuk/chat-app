3cf9df65376d683bade84cc11aa513a81fba8c50
Maciej Matwiejczuk <138388056+maciejmatwiejczuk@users.noreply.github.com>
2024-09-21 18:45:23 +0200

    Use 'UserSchema' to validate request body

--- apps/backend/src/modules/users/users.routes.ts
+++ apps/backend/src/modules/users/users.routes.ts
@@ -1,12 +1,24 @@
 import { Router } from 'express';
 import type { Request, Response } from 'express';
+import { UserSchema } from './users.schemas.js';
 
 const router = Router();
 
 router
   .route('/')
   .post((req: Request, res: Response) => {
-    res.status(201).send({ message: 'User created' });
+    const { body: user } = req;
+    console.log(user);
+
+    const result = UserSchema.safeParse(user);
+    console.log(result);
+
+    if (!result.success) {
+      console.log(result.error);
+      return res.status(400).send({ message: 'Invalid fields' });
+    }
+
+    return res.status(201).send({ message: 'User created' });
   })
   .get((req: Request, res: Response) => {
     res.send({ message: 'All users returned' });
