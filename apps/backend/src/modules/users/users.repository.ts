3081a2b4b3f0c0fc7ca8a06b0f1afcdf2b595d73
Maciej Matwiejczuk <138388056+maciejmatwiejczuk@users.noreply.github.com>
2024-09-21 23:32:42 +0200

    Create user repository

---
+++ apps/backend/src/modules/users/users.repository.ts
@@ -0,0 +1,49 @@
+import { db } from '../../database/connection.js';
+import type {
+  UserInsert,
+  UserSelect,
+  UserUpdate,
+} from '../../database/types.js';
+
+export async function createUser(user: UserInsert) {
+  return await db
+    .insertInto('user')
+    .values(user)
+    .returning(['id', 'username', 'email'])
+    .executeTakeFirstOrThrow();
+}
+
+export async function findUserById(id: number) {
+  return await db
+    .selectFrom('user')
+    .where('id', '=', id)
+    .select(['id', 'username', 'email'])
+    .executeTakeFirst();
+}
+
+export async function findUsers(criteria: Partial<UserSelect>) {
+  return await db
+    .selectFrom('user')
+    .select(['id', 'username', 'email'])
+    .$if(Boolean(criteria.username), (q) =>
+      q.where('username', '=', String(criteria.username))
+    )
+    .execute();
+}
+
+export async function updateUser(id: number, updateWith: UserUpdate) {
+  return await db
+    .updateTable('user')
+    .set(updateWith)
+    .where('id', '=', id)
+    .returning(['id', 'username', 'email'])
+    .executeTakeFirstOrThrow();
+}
+
+export async function deleteUser(id: number) {
+  return await db
+    .deleteFrom('user')
+    .where('id', '=', id)
+    .returning(['id', 'username', 'email'])
+    .executeTakeFirstOrThrow();
+}
