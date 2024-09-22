9c7b2a1dfdb9f1bd8b99bfe79482c8b3a411b61a
Maciej Matwiejczuk <138388056+maciejmatwiejczuk@users.noreply.github.com>
2024-09-22 21:06:02 +0200

    Delete 'confirmPassword' field from 'UserSchema'
    This field will be checked separately when handling user registration.

--- apps/backend/src/modules/users/users.schemas.ts
+++ apps/backend/src/modules/users/users.schemas.ts
@@ -4,5 +4,4 @@ export const UserSchema = z.object({
   username: z.string().regex(/^[a-z0-9_-]{3,20}$/),
   email: z.string().email(),
   password: z.string().min(8),
-  confirmPassword: z.string(),
 });
