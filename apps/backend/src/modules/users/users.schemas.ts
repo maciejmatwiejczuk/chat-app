e2c4bc114d7693d5d86fcc07a116633c0a1a768c
Maciej Matwiejczuk <138388056+maciejmatwiejczuk@users.noreply.github.com>
2024-09-25 01:37:57 +0200

    Update validation schemas in 'users' module
    * Change name of 'UserSchema' to 'CreateUserSchema'
    * Create 'UpdateUserSchema'
    * Create types: 'CreateUserDto' and 'UpdateUserDto'

--- apps/backend/src/modules/users/users.schemas.ts
+++ apps/backend/src/modules/users/users.schemas.ts
@@ -1,7 +1,12 @@
 import { z } from 'zod';
 
-export const UserSchema = z.object({
+export const CreateUserSchema = z.object({
   username: z.string().regex(/^[a-z0-9_-]{3,20}$/),
   email: z.string().email(),
   password: z.string().min(8),
 });
+
+export const UpdateUserSchema = CreateUserSchema.partial().strict();
+
+export type CreateUserDto = z.infer<typeof CreateUserSchema>;
+export type UpdateUserDto = z.infer<typeof UpdateUserSchema>;
