c5a3e2604bb722738acf1b8b9105376cfbc1bbb0
Maciej Matwiejczuk <138388056+maciejmatwiejczuk@users.noreply.github.com>
2024-09-25 20:47:26 +0200

    Update users schemas
    * Add function to 'CreateUserSchema' to compare 'password' to
      'confirmPassword'
    * 'UpdateUserSchema' no longer extends 'CreateUserSchema'

--- apps/backend/src/modules/users/users.schemas.ts
+++ apps/backend/src/modules/users/users.schemas.ts
@@ -1,12 +1,25 @@
 import { z } from 'zod';
 
-export const CreateUserSchema = z.object({
-  username: z.string().regex(/^[a-z0-9_-]{3,20}$/),
-  email: z.string().email(),
-  password: z.string().min(8),
-});
+export const CreateUserSchema = z
+  .object({
+    username: z.string().regex(/^[a-z0-9_-]{3,20}$/),
+    email: z.string().email(),
+    password: z.string().min(8),
+    confirmPassword: z.string(),
+  })
+  .refine((user) => user.password === user.confirmPassword, {
+    message: "Passwords don't match",
+    path: ['confirmPassword'],
+  });
 
-export const UpdateUserSchema = CreateUserSchema.partial().strict();
+export const UpdateUserSchema = z
+  .object({
+    username: z.string().regex(/^[a-z0-9_-]{3,20}$/),
+    email: z.string().email(),
+    password: z.string().min(8),
+  })
+  .partial()
+  .strict();
 
 export type CreateUserDto = z.infer<typeof CreateUserSchema>;
 export type UpdateUserDto = z.infer<typeof UpdateUserSchema>;
