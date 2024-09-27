ca348c12b02f10f80bcb527d7cb6b4331ac45c48
Maciej Matwiejczuk <138388056+maciejmatwiejczuk@users.noreply.github.com>
2024-09-27 21:35:46 +0200

    Export 'Pool'

--- apps/backend/src/database/connection.ts
+++ apps/backend/src/database/connection.ts
@@ -5,15 +5,17 @@ import { database } from '../environment.js';
 
 const { Pool } = pg;
 
+export const pool = new Pool({
+  database: database.DATABASE_NAME,
+  host: database.DATABASE_HOST,
+  user: database.DATABASE_USER,
+  password: database.DATABASE_PASSWORD,
+  port: Number(database.DATABASE_PORT),
+  max: Number(database.DATABASE_MAX_CONNECTIONS),
+});
+
 export const dialect = new PostgresDialect({
-  pool: new Pool({
-    database: database.DATABASE_NAME,
-    host: database.DATABASE_HOST,
-    user: database.DATABASE_USER,
-    password: database.DATABASE_PASSWORD,
-    port: Number(database.DATABASE_PORT),
-    max: Number(database.DATABASE_MAX_CONNECTIONS),
-  }),
+  pool,
 });
 
 export const db = new Kysely<Database>({
