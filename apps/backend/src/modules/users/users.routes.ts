08632d12d99fd5cfada1bd12337b73f394456b4c
Maciej Matwiejczuk <138388056+maciejmatwiejczuk@users.noreply.github.com>
2024-09-28 00:12:48 +0200

    Protect routes with 'authenticate' middleware

--- apps/backend/src/modules/users/users.routes.ts
+++ apps/backend/src/modules/users/users.routes.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 import type { NextFunction, Request, Response } from 'express';
 import * as UserService from './users.service.js';
+import { authenticate } from '../../middlewares/authenticate.js';
 
 const router = Router();
 
@@ -19,67 +20,79 @@ router
       next(err);
     }
   })
-  .get(async (req: Request, res: Response, next: NextFunction) => {
-    try {
-      const users = await UserService.getUsers();
+  .get(
+    authenticate,
+    async (req: Request, res: Response, next: NextFunction) => {
+      try {
+        const users = await UserService.getUsers();
 
-      res.send({
-        success: true,
-        message: 'Users returned successfuly',
-        data: { users },
-      });
-    } catch (err) {
-      next(err);
+        res.send({
+          success: true,
+          message: 'Users returned successfuly',
+          data: { users },
+        });
+      } catch (err) {
+        next(err);
+      }
     }
-  });
+  );
 
 router
   .route('/:id')
-  .get(async (req: Request, res: Response, next: NextFunction) => {
-    try {
-      const id = Number(req.params.id);
+  .get(
+    authenticate,
+    async (req: Request, res: Response, next: NextFunction) => {
+      try {
+        const id = Number(req.params.id);
 
-      const user = await UserService.getUserById(id);
+        const user = await UserService.getUserById(id);
 
-      res.send({
-        success: true,
-        message: 'User with specified id returned successfuly',
-        data: { user },
-      });
-    } catch (err) {
-      next(err);
+        res.send({
+          success: true,
+          message: 'User with specified id returned successfuly',
+          data: { user },
+        });
+      } catch (err) {
+        next(err);
+      }
     }
-  })
-  .patch(async (req: Request, res: Response, next: NextFunction) => {
-    try {
-      const id = Number(req.params.id);
-      const { body: userUpdate } = req;
+  )
+  .patch(
+    authenticate,
+    async (req: Request, res: Response, next: NextFunction) => {
+      try {
+        const id = Number(req.params.id);
+        const { body: userUpdate } = req;
 
-      const updatedUser = await UserService.updateUser(id, userUpdate);
+        const updatedUser = await UserService.updateUser(id, userUpdate);
 
-      return res.send({
-        success: true,
-        message: 'User updated successfuly',
-        data: { updated: { user: updatedUser } },
-      });
-    } catch (err) {
-      next(err);
+        return res.send({
+          success: true,
+          message: 'User updated successfuly',
+          data: { updated: { user: updatedUser } },
+        });
+      } catch (err) {
+        next(err);
+      }
     }
-  })
-  .delete(async (req: Request, res: Response, next: NextFunction) => {
-    try {
-      const id = Number(req.params.id);
+  )
+  .delete(
+    authenticate,
+    async (req: Request, res: Response, next: NextFunction) => {
+      try {
+        const id = Number(req.params.id);
 
-      const deletedUser = await UserService.deleteUser(id);
+        const deletedUser = await UserService.deleteUser(id);
 
-      res.send({
-        success: true,
-        message: 'User deleted successfuly',
-        data: { deleted: { user: deletedUser } },
-      });
-    } catch (err) {
-      next(err);
+        res.send({
+          success: true,
+          message: 'User deleted successfuly',
+          data: { deleted: { user: deletedUser } },
+        });
+      } catch (err) {
+        next(err);
+      }
     }
-  });
+  );
 
 export default router;
