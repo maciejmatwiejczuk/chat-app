f4de5d9f3f25077fe09e502d0c7ff5f1305133e0
Maciej Matwiejczuk <138388056+maciejmatwiejczuk@users.noreply.github.com>
2024-10-11 00:13:13 +0200

    Render message about successful registration

--- apps/frontend/src/pages/SignIn/SignIn.tsx
+++ apps/frontend/src/pages/SignIn/SignIn.tsx
@@ -1,32 +1,64 @@
-import { useState } from 'react';
-import { Link } from 'react-router-dom';
-import TextInput from '../../components/_common/TextInput/TextInput';
+import { Link, useLocation } from 'react-router-dom';
+import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { LoginSchema, LoginDto } from '@chat-app/_common/schemas/sessions';
 import styles from './sign-in.module.css';
 import Button from '../../components/_common/Button/Button';
+import FormInput from '../../components/_common/FormInput/FormInput';
 
 function SignIn() {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isSubmitting },
+  } = useForm<LoginDto>({
+    mode: 'onBlur',
+    resolver: zodResolver(LoginSchema),
+  });
+
+  const location = useLocation();
+
+  async function onSubmit(data: LoginDto) {
+    await new Promise((resolve) => setTimeout(resolve, 2000));
+  }
 
   return (
     <div className={styles.container}>
-      <form className={styles.form}>
+      <form onSubmit={handleSubmit(onSubmit)} className={styles.form}>
         <h2 className={styles.heading}>Sign In</h2>
 
+        {location.state?.isRegistered && (
+          <div className={styles.infoBox}>
+            Your account was successfully registered. &nbsp; You can login now.
+          </div>
+        )}
+
         <div className={styles.inputWrapper}>
-          <TextInput value={username} setValue={setUsername} label="Username" />
+          <FormInput
+            {...register('username')}
+            label="Username"
+            error={errors.username?.message}
+            isRequired={true}
+          />
         </div>
         <div className={styles.inputWrapper}>
-          <TextInput
-            value={password}
-            setValue={setPassword}
+          <FormInput
+            {...register('password')}
             label="Password"
             type="password"
+            error={errors.password?.message}
+            isRequired={true}
           />
         </div>
 
         <div className={styles.buttonWrapper}>
-          <Button title="Sign in" size="small" type="fill" isWide={true} />
+          <Button
+            title="Sign in"
+            size="small"
+            type="fill"
+            isWide={true}
+            isDisabled={isSubmitting}
+          />
         </div>
 
         <p className={styles.bottomText}>
