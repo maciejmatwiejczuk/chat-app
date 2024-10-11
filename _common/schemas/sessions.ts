3d75795c22d19d2f61cf230b957e06523c39301b
Maciej Matwiejczuk <138388056+maciejmatwiejczuk@users.noreply.github.com>
2024-10-11 23:13:08 +0200

    Add additional validators to LoginSchema
    These validators were added to prevent sending request that would fail
    either way because of bad input format.

--- _common/schemas/sessions.ts
+++ _common/schemas/sessions.ts
@@ -1,8 +1,11 @@
 import { z } from 'zod';
 
 export const LoginSchema = z.object({
-  username: z.string(),
-  password: z.string(),
+  username: z
+    .string()
+    .min(1)
+    .regex(/^[a-zA-Z0-9_-]{3,20}$/),
+  password: z.string().min(1).min(8),
 });
 
 export type LoginDto = z.infer<typeof LoginSchema>;
