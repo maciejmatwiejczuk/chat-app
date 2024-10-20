3439b22169485f581ddd7495821ec5ea04d122af
Maciej Matwiejczuk <138388056+maciejmatwiejczuk@users.noreply.github.com>
2024-10-20 16:52:11 +0200

    Use 'onLogin' from 'AuthContext' in 'useLogin'

--- apps/frontend/src/api/sessions.ts
+++ apps/frontend/src/api/sessions.ts
@@ -1,11 +1,16 @@
 import { useMutation } from '@tanstack/react-query';
 import { LoginDto } from '@chat-app/_common/schemas/sessions.ts';
 import axios from '../config/axios.ts';
+import { useContext } from 'react';
+import { AuthContext } from '../context/AuthContext.tsx';
 
 const ENDPOINT = 'login';
 
 export function useLogin() {
+  const { onLogin } = useContext(AuthContext);
+
   return useMutation({
     mutationFn: (data: LoginDto) => axios.post(ENDPOINT, data),
+    onSuccess: () => onLogin && onLogin(),
   });
 }
