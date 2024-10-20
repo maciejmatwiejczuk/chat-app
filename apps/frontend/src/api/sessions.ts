05a79e2056cff6c6c36de62e135adf8246808c67
Maciej Matwiejczuk <138388056+maciejmatwiejczuk@users.noreply.github.com>
2024-10-20 23:26:27 +0200

    Create 'useLogout' hook

--- apps/frontend/src/api/sessions.ts
+++ apps/frontend/src/api/sessions.ts
@@ -4,13 +4,24 @@ import axios from '../config/axios.ts';
 import { useContext } from 'react';
 import { AuthContext } from '../context/AuthContext.tsx';
 
-const ENDPOINT = 'login';
+const LOGIN_ENDPOINT = 'login';
 
 export function useLogin() {
   const { onLogin } = useContext(AuthContext);
 
   return useMutation({
-    mutationFn: (data: LoginDto) => axios.post(ENDPOINT, data),
+    mutationFn: (data: LoginDto) => axios.post(LOGIN_ENDPOINT, data),
     onSuccess: () => onLogin && onLogin(),
+  });
+}
+
+const LOGOUT_ENDPOINT = 'logout';
+
+export function useLogout() {
+  const { onLogout } = useContext(AuthContext);
+
+  return useMutation({
+    mutationFn: () => axios.post(LOGOUT_ENDPOINT),
+    onSuccess: () => onLogout && onLogout(),
   });
 }
