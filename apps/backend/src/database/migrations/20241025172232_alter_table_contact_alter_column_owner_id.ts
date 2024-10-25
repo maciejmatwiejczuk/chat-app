5ceb5e63b2f065a6f46c2d18013eadb5a9105687
Maciej Matwiejczuk <138388056+maciejmatwiejczuk@users.noreply.github.com>
2024-10-25 19:31:47 +0200

    Create migration updating 'owner_id' in 'contact'
    This migration updates 'owner_id' row in 'contact' table setting
    NOT NULL constraint.

---
+++ apps/backend/src/database/migrations/20241025172232_alter_table_contact_alter_column_owner_id.ts
@@ -0,0 +1,16 @@
+import type { Kysely } from 'kysely';
+import type { Database } from '../types.js';
+
+export async function up(db: Kysely<Database>): Promise<void> {
+  await db.schema
+    .alterTable('contact')
+    .alterColumn('owner_id', (col) => col.setNotNull())
+    .execute();
+}
+
+export async function down(db: Kysely<Database>): Promise<void> {
+  await db.schema
+    .alterTable('contact')
+    .alterColumn('owner_id', (col) => col.dropNotNull())
+    .execute();
+}
