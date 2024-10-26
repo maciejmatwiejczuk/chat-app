28a9ee220379c253a6a2caea3c2139066347d35d
Maciej Matwiejczuk <138388056+maciejmatwiejczuk@users.noreply.github.com>
2024-10-26 19:44:20 +0200

    Create service of 'invitations' module
    The service methods call previously implemented methods from the
    repository.

---
+++ apps/backend/src/modules/invitations/invitations.service.ts
@@ -0,0 +1,31 @@
+import AppError from '../../utils/AppError.js';
+import * as InvitationRepository from './invitations.repository.js';
+
+export async function createInvitation(senderId: number, receiverId: number) {
+  const createdInvitation = await InvitationRepository.createInvitation({
+    sender_id: senderId,
+    receiver_id: receiverId,
+  });
+
+  return createdInvitation;
+}
+
+export async function getInvitationById(id: number) {
+  const invitation = await InvitationRepository.findInvitationById(id);
+
+  if (!invitation) {
+    throw new AppError('not_found', 404, 'Invitation not found', true);
+  }
+
+  return invitation;
+}
+
+export async function deleteInvitation(id: number) {
+  const deletedInvitation = await InvitationRepository.deleteInvitation(id);
+
+  if (!deletedInvitation) {
+    throw new AppError('not_found', 404, 'Invitation not found', true);
+  }
+
+  return deletedInvitation;
+}
