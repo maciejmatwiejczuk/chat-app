ef4c400844df785f9878d68fad13a5333e1b1738
Maciej Matwiejczuk <138388056+maciejmatwiejczuk@users.noreply.github.com>
2024-11-16 18:26:12 +0100

    Install and set up React Query Devtools

--- apps/frontend/src/main.tsx
+++ apps/frontend/src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import ChatPage from './pages/ChatPage/ChatPage';
 import SignUp from './pages/SignUp/SignUp';
 import SignIn from './pages/SignIn/SignIn';
@@ -54,6 +55,7 @@ const queryClient = new QueryClient();
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={false} />
       <RouterProvider router={router} />
     </QueryClientProvider>
   </StrictMode>
