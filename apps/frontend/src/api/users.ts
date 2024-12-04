5a9eb9121dc739991ab563905bbd522b0e4c8472
Maciej Matwiejczuk <138388056+maciejmatwiejczuk@users.noreply.github.com>
2024-12-04 20:10:02 +0100

    Implement dynamic user search
    Dynamic user search calls api when user stops
    typing trying to get results matching the
    username user's searching for. The api returns
    10 users at a time, subsequent pages of users
    are retrieved as the users scrolls down (infinite
    scroll). Intersection Observer API and infite
    queries from React Query were used to implement
    this behavior.

--- apps/frontend/src/api/users.ts
+++ apps/frontend/src/api/users.ts
@@ -1,4 +1,4 @@
-import { useMutation } from '@tanstack/react-query';
+import { useInfiniteQuery, useMutation } from '@tanstack/react-query';
 import { CreateUserDto } from '@chat-app/_common/schemas/users';
 import axios from '../config/axios.ts';
 
@@ -7,5 +7,25 @@ const ENDPOINT = 'users';
 export function useCreateUser() {
   return useMutation({
     mutationFn: (data: CreateUserDto) => axios.post(ENDPOINT, data),
+  });
+}
+
+export function useGetUsers(search?: string) {
+  return useInfiniteQuery({
+    queryKey: ['users', search],
+    queryFn: async ({ pageParam }) => {
+      const response = await axios.get(ENDPOINT, {
+        params: {
+          page: pageParam,
+          username: search,
+        },
+      });
+
+      return response.data;
+    },
+    initialPageParam: 1,
+    getNextPageParam: (lastPage, _pages, lastPageParam) => {
+      return lastPage.users.length > 0 ? lastPageParam + 1 : undefined;
+    },
   });
 }
--- apps/frontend/src/components/SideMenu/ChatList/ChatList.tsx
+++ apps/frontend/src/components/SideMenu/ChatList/ChatList.tsx
@@ -1,9 +1,13 @@
 import { useParams, Link } from 'react-router-dom';
 import { MagnifyingGlass } from '@phosphor-icons/react';
-import { contacts, users } from '../../../data';
+import { contacts } from '../../../data';
 import Avatar from '../../_common/Avatar/Avatar';
 import type { OptionValue } from '../SideMenu';
 import styles from './chat-list.module.css';
+import { useGetUsers } from '../../../api/users';
+import { forwardRef, useCallback, useRef } from 'react';
+import { useDebounce } from '../../../hooks/useDebounce';
+import Loader from '../../_common/Loader/Loader';
 
 interface ChatListProps {
   dropdownSelection: OptionValue;
@@ -17,63 +21,161 @@ function ChatList({ dropdownSelection, searchValue }: ChatListProps) {
     return Number(chatId) === id;
   }
 
+  const debouncedSearch = useDebounce(searchValue, 500);
+
   function renderItems() {
     if (dropdownSelection === 'my_contacts') {
-      if (searchValue) {
+      if (debouncedSearch) {
         const foundContacts = contacts.filter((contact) =>
-          contact.username.toLowerCase().includes(searchValue.toLowerCase())
+          contact.username.toLowerCase().includes(debouncedSearch.toLowerCase())
         );
 
-        return foundContacts.map((contact) => (
-          <ChatListItem
-            socketId={contact.socketId}
-            name={contact.username}
-            lastMessage={contact.last_message}
-            isOpen={isChatOpen(contact.socketId)}
-          />
-        ));
+        return (
+          <div className={styles.container}>
+            <ul className={styles.list}>
+              {foundContacts.map((contact) => (
+                <ChatListItem
+                  socketId={contact.socketId}
+                  name={contact.username}
+                  lastMessage={contact.last_message}
+                  isOpen={isChatOpen(contact.socketId)}
+                />
+              ))}
+            </ul>
+          </div>
+        );
       }
 
-      return contacts.map((contact) => (
-        <ChatListItem
-          socketId={contact.socketId}
-          name={contact.username}
-          lastMessage={contact.last_message}
-          isOpen={isChatOpen(contact.socketId)}
-        />
-      ));
+      return (
+        <div className={styles.container}>
+          <ul className={styles.list}>
+            {contacts.map((contact) => (
+              <ChatListItem
+                socketId={contact.socketId}
+                name={contact.username}
+                lastMessage={contact.last_message}
+                isOpen={isChatOpen(contact.socketId)}
+              />
+            ))}
+          </ul>
+        </div>
+      );
     } else {
-      if (searchValue) {
-        const foundUsers = users
-          .filter((user) => !contacts.find((contact) => contact.id === user.id))
-          .filter((user) =>
-            user.username.toLowerCase().includes(searchValue.toLowerCase())
-          );
-
-        return foundUsers.map((user) => (
-          <ChatListItem
-            socketId={user.socketId}
-            name={user.username}
-            isOpen={isChatOpen(user.socketId)}
-          />
-        ));
+      if (debouncedSearch) {
+        return <SearchUsersList searchValue={debouncedSearch} />;
       }
 
-      return null;
-    }
-  }
-
-  return (
-    <div className={styles.container}>
-      {dropdownSelection === 'find_users' && !searchValue ? (
+      return (
         <p className={styles.placeholder}>
           Use{' '}
           <MagnifyingGlass weight="bold" className={styles.placeholderIcon} />{' '}
           <span>search</span> above to find users
         </p>
-      ) : (
-        <ul className={styles.list}>{renderItems()}</ul>
-      )}
+      );
+    }
+  }
+
+  return renderItems();
+}
+
+interface SearchUsersListProps {
+  searchValue?: string;
+}
+
+function SearchUsersList({ searchValue }: SearchUsersListProps) {
+  const {
+    data,
+    fetchNextPage,
+    isFetchingNextPage,
+    hasNextPage,
+    isPending,
+    isError,
+  } = useGetUsers(searchValue);
+
+  const intObserver = useRef<IntersectionObserver>();
+  const observerRootRef = useRef<HTMLDivElement>(null);
+  const lastUserRef = useCallback(
+    (user: HTMLLIElement) => {
+      if (isFetchingNextPage) {
+        return;
+      }
+
+      if (intObserver.current) {
+        intObserver.current.disconnect();
+      }
+
+      intObserver.current = new IntersectionObserver(
+        (users) => {
+          console.log(users);
+          if (users[0].isIntersecting && hasNextPage) {
+            fetchNextPage();
+          }
+        },
+        {
+          root: observerRootRef.current,
+        }
+      );
+
+      if (user) intObserver.current.observe(user);
+    },
+    [fetchNextPage, isFetchingNextPage, hasNextPage, observerRootRef]
+  );
+
+  const { chatId } = useParams();
+
+  function isChatOpen(id: number) {
+    return Number(chatId) === id;
+  }
+
+  if (isPending) {
+    return (
+      <div className={styles.loaderWrapper}>
+        <Loader size="medium" />
+      </div>
+    );
+  }
+
+  if (isError) {
+    return <p className={styles.placeholder}>Failed to find users</p>;
+  }
+
+  if (data.pages[0].users.length === 0) {
+    return (
+      <p className={styles.placeholder}>
+        No users found matching the searched name
+      </p>
+    );
+  }
+
+  const items = data?.pages.map(({ users }) => {
+    return users.map((user: { id: number; username: string }, i: number) => {
+      if (i === users.length - 1) {
+        return (
+          <ChatListItem
+            socketId={user.id}
+            name={user.username}
+            isOpen={isChatOpen(user.id)}
+            ref={lastUserRef}
+          />
+        );
+      }
+
+      return (
+        <ChatListItem
+          socketId={user.id}
+          name={user.username}
+          isOpen={isChatOpen(user.id)}
+        />
+      );
+    });
+  });
+
+  return (
+    <div className={styles.container} ref={observerRootRef}>
+      <ul className={styles.list}>{items}</ul>
+      <div className={styles.loaderWrapper}>
+        {isFetchingNextPage && <Loader size="medium" />}
+      </div>
     </div>
   );
 }
@@ -85,27 +187,24 @@ interface ChatListItemProps {
   isOpen: boolean;
 }
 
-function ChatListItem({
-  socketId,
-  name,
-  lastMessage,
-  isOpen,
-}: ChatListItemProps) {
-  const listItemStyle = isOpen
-    ? `${styles.listItem} ${styles.listItemOpen}`
-    : styles.listItem;
+const ChatListItem = forwardRef<HTMLLIElement, ChatListItemProps>(
+  ({ socketId, name, lastMessage, isOpen }: ChatListItemProps, ref) => {
+    const listItemStyle = isOpen
+      ? `${styles.listItem} ${styles.listItemOpen}`
+      : styles.listItem;
 
-  return (
-    <li className={listItemStyle}>
-      <Link to={`chat/${socketId}`}>
-        <Avatar />
-        <div>
-          <h4 className={styles.chatName}>{name}</h4>
-          <p className={styles.lastMessage}>{lastMessage}</p>
-        </div>
-      </Link>
-    </li>
-  );
-}
+    return (
+      <li className={listItemStyle} ref={ref} key={socketId}>
+        <Link to={`chat/${socketId}`}>
+          <Avatar />
+          <div>
+            <h4 className={styles.chatName}>{name}</h4>
+            <p className={styles.lastMessage}>{lastMessage}</p>
+          </div>
+        </Link>
+      </li>
+    );
+  }
+);
 
 export default ChatList;
