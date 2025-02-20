8564176b7d8ef2880cc6c77c61458119e779582f
Maciej Matwiejczuk <138388056+maciejmatwiejczuk@users.noreply.github.com>
2025-02-20 21:15:26 +0100

    Add production origin to cors config

--- apps/backend/src/config/cors.ts
+++ apps/backend/src/config/cors.ts
@@ -1,4 +1,7 @@
 export const corsConfig = {
-  origin: 'http://localhost:5173',
+  origin: [
+    'http://localhost:5173',
+    'https://chat-app-frontend-l6l1.onrender.com',
+  ],
   credentials: true,
 } as const;
